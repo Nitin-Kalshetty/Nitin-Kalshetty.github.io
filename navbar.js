07b53ee7ec1a72d42e45b9967d528e3dc43715dc
Rocker Ni3 <nitin939426@gmail.com>
2022-10-19 19:40:27 +0530

    added home section

--- navbar.js
+++ navbar.js
@@ -14,3 +14,69 @@ menu.addEventListener("click",function(){
     }
 })
 
+let name = document.querySelector(".name");
+let designation_q = document.querySelector(".designation_query");
+let designation= document.querySelector(".designation");
+let resume= document.querySelector(".resume");
+let id;
+let nameData = "Nitin Kalshetty";
+let i=0;
+let str="";
+    setTimeout(() => {
+        id = setInterval(function(){
+            str+=nameData[i++];
+            name.innerHTML = str;
+            if(i==nameData.length){
+                closeId();
+                queryDesignation();
+            }
+        },300)
+    }, 1000);
+
+function closeId(){
+    clearInterval(id);
+}
+
+function queryDesignation(){
+    nameData="Hello, I'm a";
+    i=0;
+    str=""
+    id = setInterval(function(){
+        str+=nameData[i++];
+        designation_q.innerHTML = str;
+        if(i==nameData.length){
+            closeId();
+            DesignationJava()
+        }
+    },300)
+}
+function DesignationJava(){
+    nameData="Java Backend Developer.";
+    i=0;
+    str=""
+    id = setInterval(function(){
+        str+=nameData[i++];
+        designation.innerHTML = str;
+        if(i==nameData.length){
+            closeId();
+            resumeWork()
+        }
+    },300)
+}
+function resumeWork(){
+    nameData="Resume";
+    i=0;
+    str=""
+    id = setInterval(function(){
+        str+=nameData[i++];
+        resume.innerHTML = str;
+        if(i==nameData.length){
+            closeId();
+            resume.setAttribute("id","resumeID")
+        }
+    },300)
+}
+
+function closeId(){
+    clearInterval(id);
+};
